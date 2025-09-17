6cdd9db5cbc99be6a90a64d5a9950de05a36388d
eanassu <eanassu@gmail.com>
2025-09-17 20:44:09 -0300

    variável não usada

--- lib/tip_calculator.dart
+++ lib/tip_calculator.dart
@@ -12,7 +12,7 @@ class _TipCalculatorState extends State<TipCalculator> {
   double _valor = 0.0;
   double _gorjeta = 0.0;
   double _valorTotal = 0.0;
-  final bool _year2023 = true;
+
   final TextEditingController _controller1 = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -60,7 +60,6 @@ class _TipCalculatorState extends State<TipCalculator> {
             style: TextStyle(fontSize: 20),
           ),
           Slider(
-              year2023: _year2023,
               value: _currentSliderValue,
               max: 30,
               onChanged: (double value) {
